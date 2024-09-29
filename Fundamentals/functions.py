9bf33581ad7ce3a97eda6cbc2b4970506f4a2e68
KevinLatino <latinokevin9@gmail.com>
2024-09-28 19:57:25 -0600

    functions are added now

--- Fundamentals/functions.py
+++ Fundamentals/functions.py
@@ -5,7 +5,7 @@ def add(num1, num2):
 add(num1 = 3, num2 = 5)
 
 #example of a lambda function
-add = lambda num1, num2 : print(num1 + num2);
+add = lambda num1, num2 : print(num1 + num2)
 
 add(4, 5)
 
